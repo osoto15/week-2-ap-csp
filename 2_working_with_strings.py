da3028d71cc504d08c07f3fe89bb8dfc658656f3
osoto15 <osoto15@cps.edu>
2025-10-28 09:33:29 -0500

    Update 2_working_with_strings.py

--- 2_working_with_strings.py
+++ 2_working_with_strings.py
@@ -4,8 +4,8 @@
 # ----------------------------------------
 
 # Strings are sequences of characters enclosed in quotes (' ' or " ")
-greeting = "Hello"
-name = "World"
+greeting = "Hello" #sring data types
+name = "World" #string data types
 
 # ----------------------------------------
 # Basic String Operations
@@ -20,63 +20,68 @@ print("Concatenated String:", message)  # Output: Hello World
 # ----------------------------------------
 
 phrase = "Python is FUN!"
-
-# Convert all characters to lowercase
+name = "Olivia"
+phrase2 = "SUPERCAGEFRAGILISTICIOUS"
+# # Convert all characters to lowercase
 print("Lowercase:", phrase.lower())  # Output: python is fun!
+print("Name Lowercase:", name.lower())
+print("phrase2 Lowercase:", phrase2.lower())
 
-# Convert all characters to uppercase
+# # Convert all characters to uppercase
 print("Uppercase:", phrase.upper())  # Output: PYTHON IS FUN!
+print("Name Uppercase:", name.upper())
 
-# Check if all characters are uppercase
+# # Check if all characters are uppercase
 print("Is Uppercase?", phrase.isupper())  # Output: False
+print("Is Name Uppercase?", name.isupper())
+print ("Is Phrase2 Uppercase?", phrase2.isupper())
+# # Find the length of the string
+# print("Length of phrase:", len(phrase))  # Output: 14
 
-# Find the length of the string
-print("Length of phrase:", len(phrase))  # Output: 14
+# # ----------------------------------------
+# # 3. Indexing and Slicing
+# # ----------------------------------------
 
-# ----------------------------------------
-# 3. Indexing and Slicing
-# ----------------------------------------
+# # Indexing: Access characters by position (0-based index)
+# print("First character:", phrase[0])  # Output: P
+# print("Last character:", phrase[-1])  # Output: !
 
-# Indexing: Access characters by position (0-based index)
-print("First character:", phrase[0])  # Output: P
-print("Last character:", phrase[-1])  # Output: !
+# # Slicing: Get a range of characters (start inclusive, end exclusive)
+# print("Characters 1 to 4:", phrase[1:4])  # Output: yth
 
-# Slicing: Get a range of characters (start inclusive, end exclusive)
-print("Characters 1 to 4:", phrase[1:4])  # Output: yth
-
-# Example combining everything:
-print("Formatted Example:", (greeting + " " + name + "!").upper())
-# Output: HELLO WORLD!
+# # Example combining everything:
+# print("Formatted Example:", (greeting + " " + name + "!").upper())
+# # Output: HELLO WORLD!
 
 
-# ----------------------------------------
-# 7. Strings: Advanced Concepts
-# ----------------------------------------
+# # ----------------------------------------
+# # 7. Strings: Advanced Concepts
+# # ----------------------------------------
 
-# Creating Strings: use single or double quotes
-greeting1 = 'Hello'
-greeting2 = "Hi there"
+# # Creating Strings: use single or double quotes
+# greeting1 = 'Hello'
+# greeting2 = "Hi there"
 
-# Printing Strings
-print(greeting1)
-print(greeting2)
+# # Printing Strings
+# print(greeting1)
+# print(greeting2)
 
-# ----------------------------------------
-# String Methods
-# ----------------------------------------
+# # ----------------------------------------
+# # String Methods
+# # ----------------------------------------
 
-sentence = "Python is fun to learn"
+# sentence = "Python is fun to learn"
 
-# .split(): Splits the string into a list of words
-words = sentence.split()
-print("Split result:", words)
+# # .split(): Splits the string into a list of words
+# words = sentence.split()
+# print("Split result:", words)
 
-# .format(): Allows inserting values into strings using {}
-name = "Marvin"
-age = 35
-intro = "My name is {} and I am {} years old.".format(name, age)
-print(intro)
+# # .format(): Allows inserting values into strings using {}
+# name = "Marvin"
+# age = 35
+# intro = "My name is {} and I am {} years old.".format(name, age)
+# print(intro)
 
-# You can also use f-strings (Python 3.6+)
-intro_fstring = f"My name is {name} and I am {age} years old."
-print(intro_fstring)
+# # You can also use f-strings (Python 3.6+)
+# intro_fstring = f"My name is {name} and I am {age} years old."
+# print(intro_fstring)
